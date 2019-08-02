ab6fef7fce088a79289d68107a54a46013ff818c
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-08-02 20:38:05 +0600

    basic structures of first weeks study

--- swiftStarted.playground/Contents.swift
+++ swiftStarted.playground/Contents.swift
@@ -1,11 +1,33 @@
 import UIKit
 
+//###########  Data types #################
+
+//########### func #################
 
 func addNum(num1: Int, num2: Int) -> Int {
     return num1+num2
 }
-
 let data = addNum(num1: 100, num2: 11)
+var arr = [1,2,3]
+var emptyArr = [Int]()
+emptyArr.append(4)
 
-print(data)
+print(emptyArr)
 
+//########### Array #################
+
+for char in arr {
+    if char == 3 {
+        print(char)
+    } else if char == 1 {
+        print("Not a numer ? :D")
+    }
+}
+
+//###########  Switch #################
+
+//###########  Object #################
+
+//###########  Class #################
+
+//###########  Class #################
