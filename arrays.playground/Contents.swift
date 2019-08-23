5c15d86ea9b0cc8a7d354b445e765021a515c442
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-08-23 21:44:00 +0600

    sum of numbers

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -2,17 +2,22 @@ import UIKit
 
 //########### Array #################
 
-var arr = [1,2,3,5,7,8]
-var emptyArr = [Int]()
-emptyArr.append(4)
-print(emptyArr)
-
-for char in arr {
-    if char == 3 || char < 2 {
-        print(char)
-    } else if char == 4 {
-        print("Not a numer ? :D")
-    } else {
-        print("nada")
-    }
+//var arr = [1,2,3,5,7,8]
+//
+//for char in arr {
+//    if char == 1 || char == 2 {
+//        print(char)
+//    } else if char == 90 {
+//        print("Not a numer ? :D")
+//    } else if char == 8 && char > 0  {
+//        print("Not a numer ? :D")
+//    } else {
+//        print("nada")
+//    }
+//}
+var sum = 0
+for char in 1...5 {
+    sum +=  char
 }
+print(sum)
+
---
+++ loops.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var arr = [1,2,3,5,7,8]
+var emptyArr = [Int]()
+emptyArr.append(4)
+print(emptyArr)
