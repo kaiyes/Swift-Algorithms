2e98ae294dbd94943efb27ac1da10673e6c41752
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-09-05 20:14:36 +0600

    added reactXP

--- swiftStarted.playground/Contents.swift
+++ swiftStarted.playground/Contents.swift
@@ -1,38 +1,40 @@
 import UIKit
 
-//###########  math #################
-// print needs double " instead of '
-print("print needs double")
-pow(2, 2)
-print(2*2*2)
-sqrt(4)
-ceil(4.2)
-floor(4.2)
-var a:Int = 1
-a+=1
-print(a)
 
 //###########  Data types #################
-let str = "Today my friends kid died. She was"
-let age = 5
-let daysInHospital = 3
-print(str,String(age))
-print("She died after being in the hospital for \(daysInHospital) days")
+let x = "jahanara bith"
+let y = "Kaiyes Ansary"
+let age = 28
 
-//########### func #################
-func addNum(num1: Int, num2: Int) -> Int {
-    return num1+num2
-}
-let data = addNum(num1: 100, num2: 11)
-print(data)
+print(x,y)
 
 
-//###########  Switch #################
-let number: Int = 1
-switch number {
-case 110:
-    print("Number is 110")
-default:
-    print("Otherwise, do something else.")
-}
 
+////########### func #################
+//func addNum(num1: Int, num2: Int) -> Int {
+//    return num1+num2
+//}
+//let data = addNum(num1: 100, num2: 11)
+//print(data)
+//
+//
+////###########  Switch #################
+//let number: Int = 1
+//switch number {
+//case 110:
+//    print("Number is 110")
+//default:
+//    print("Otherwise, do something else.")
+//}
+//
+////###########  math #################
+//// print needs double " instead of '
+//print("print needs double")
+//pow(2, 2)
+//print(2*2*2)
+//sqrt(4)
+//ceil(4.2)
+//floor(4.2)
+//var a:Int = 1
+//a+=1
+//print(a)
