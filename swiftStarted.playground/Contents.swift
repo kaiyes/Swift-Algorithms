a4c9c4444cbf0b64d6b3061af135935b4d0cb677
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-09-07 13:56:22 +0600

    took out toy code

--- swiftStarted.playground/Contents.swift
+++ swiftStarted.playground/Contents.swift
@@ -1,40 +1,30 @@
 import UIKit
 
-
-//###########  Data types #################
-let x = "jahanara bith"
-let y = "Kaiyes Ansary"
-let age = 28
-
-print(x,y)
-
+//########### func #################
+func addNum(num1: Int, num2: Int) -> Int {
+    return num1+num2
+}
+let data = addNum(num1: 100, num2: 11)
+print(data)
 
 
-////########### func #################
-//func addNum(num1: Int, num2: Int) -> Int {
-//    return num1+num2
-//}
-//let data = addNum(num1: 100, num2: 11)
-//print(data)
-//
-//
-////###########  Switch #################
-//let number: Int = 1
-//switch number {
-//case 110:
-//    print("Number is 110")
-//default:
-//    print("Otherwise, do something else.")
-//}
-//
-////###########  math #################
-//// print needs double " instead of '
-//print("print needs double")
-//pow(2, 2)
-//print(2*2*2)
-//sqrt(4)
-//ceil(4.2)
-//floor(4.2)
-//var a:Int = 1
-//a+=1
-//print(a)
+//###########  Switch #################
+let number: Int = 1
+switch number {
+case 110:
+    print("Number is 110")
+default:
+    print("Otherwise, do something else.")
+}
+
+//###########  math #################
+// print needs double " instead of '
+print("print needs double")
+pow(2, 2)
+print(2*2*2)
+sqrt(4)
+ceil(4.2)
+floor(4.2)
+var a:Int = 1
+a+=1
+print(a)
