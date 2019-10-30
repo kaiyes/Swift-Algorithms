4d79acb2a3a3c899a1d0309aa43a8d1d74481e2c
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-10-30 13:11:28 +0600

    ternery

---
+++ bools.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let bride = 33
+let groom = 21
+
+
+bride < 18 ?  "No marrige due to bride beign underage" : groom > bride ?   "marry them 1" :  bride > groom ? "islamically ok, but socially not" : "marry them 2"
+
+
