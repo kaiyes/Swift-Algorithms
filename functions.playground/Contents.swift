3c08d2d928cedb16db3fafa96cc6b2f8889b01bd
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-11-02 19:17:58 +0600

    added function overloading

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -1,11 +1,8 @@
 import UIKit
 
-
 func printThat(x:Int) {
     print("\(5 * x)")
 }
-
-printThat(x:2)
 
 func dualParam (a:Int=1,b:String) {
     print("Ertugul believed there would be \(a) great leader under whom he would serve! \(b)")
@@ -19,8 +16,34 @@ func multiply(_ factor1: Int, _ factor2:Int) -> Int {
 
 multiply(2,3)
 
-func test_(factor1: Int, factor2:Int) -> Int {
-    return factor1*factor2
+let str = "Ertugul believed there would be a great leader under whom he would serve!"
+
+
+//func test_(factor1: Int, factor2:Int) -> Int {
+//    return factor1*factor2
+//}
+//
+//test_(1, 2)
+
+// return tuples
+func testReturningTuples(_ nameData: String, ageData:Int) -> (name: String, age: Int) {
+    return (nameData, ageData)
 }
 
-test_(1, 2)
+let res = testReturningTuples( "kaiyes", ageData: 21)
+
+// function overloading = same function with different params
+
+func testOverLoading (_ oneParam: Int) {
+    print(oneParam)
+}
+
+func testOverLoading (_ oneParam: Int, _ secondParam: String) {
+    print (secondParam, oneParam)
+}
+
+func testOverLoading (_ thirdParam: String) {
+    print(thirdParam)
+}
+
+testOverLoading("print me")
