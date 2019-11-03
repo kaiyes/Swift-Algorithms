b59198f3cd6f85fd7061378824ff5b9dd519931c
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-11-03 15:46:41 +0600

    Added more array methods

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -2,28 +2,32 @@ import UIKit
 
 //arrays can be of one type
 //non-empty arrays can be implicit
-var arrOfNumbers = [1,2,3]
+var arr = [1,2,3]
 var arrOfStrings = [ "A","B","C"]
 
 // empty arrays gotta be explicit
 var emptyArrExample: [Int] = []
 
 //########### Array Methods #################
-arrOfNumbers.append(8)
-arrOfNumbers += [5,7]
+arr.append(8)
+arr += [5,7]
 //properties
-arrOfNumbers.count
-arrOfNumbers.capacity
-arrOfNumbers.isEmpty
-arrOfNumbers.first
-arrOfNumbers.last
-arrOfNumbers.max()
-arrOfNumbers.min()
-arrOfNumbers[0]
-let slicedArray = arrOfNumbers[0...2]
-arrOfNumbers.removeLast()
-arrOfNumbers.removeFirst()
-arrOfNumbers.contains(2)
-arrOfNumbers.contains(100)
+arr.count
+arr.capacity
+arr.isEmpty
+arr.first
+arr.last
+arr.max()
+arr.min()
+arr[0]
+let slicedArray = arr[0...2]
+arr.removeLast()
+arr.removeFirst()
+arr.contains(2)
+arr.contains(100)
+arr.insert(199, at: 2)
+arr
+arr.remove(at: 2)
+
 //arrOfnumbers.removeAll()
 
