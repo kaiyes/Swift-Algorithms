5546a4331d3c47ccf6bc75c185a696dac5754b81
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-11-07 09:55:57 +0600

    dict

--- dictionary.playground/Contents.swift
+++ dictionary.playground/Contents.swift
@@ -1,1 +1,29 @@
 import UIKit
+
+var conquerers = [
+    "first" : "Sulayman Shah",
+    "second": "Ertugrul",
+    "third" : "Osman",
+    "fourth": "who?"
+]
+
+conquerers["first"]
+conquerers["sec"]
+conquerers.count
+conquerers.isEmpty
+
+//nil coelsion
+let fatih = conquerers["Fatih"] ?? "Fatih hasn't been born yet"
+
+conquerers.updateValue("Mohammad fatih", forKey: "fourth")
+conquerers["fourth"] = "someone else"
+
+//conquerers.removeValue(forKey: "fourth")
+//print(conquerers)
+conquerers["fourth"] = nil
+//print(conquerers)
+
+for (index, item) in conquerers {
+    print(index, item)
+}
+
